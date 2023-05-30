6d4464d52a22b0d90a76389c5609e3ecfccec8bc
Cafei-xx <128898160+Cafei-xx@users.noreply.github.com>
2023-05-30 16:43:04 +0100

    Update precision.c
    Added comments

--- precision.c
+++ precision.c
@@ -5,6 +5,7 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ * it is used to control the max number of characters printed for certain format specifiers.
  *
  * Return: Precision.
  */
